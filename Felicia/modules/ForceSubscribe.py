fd47c884a8aa3672e1d68f0fe8186fd4d86a5d54
The Quaint <87853928+The-Quaint@users.noreply.github.com>
2022-01-03 16:28:28 +0530

    Update ForceSubscribe.py

--- Felicia/modules/ForceSubscribe.py
+++ Felicia/modules/ForceSubscribe.py
@@ -1,4 +1,4 @@
-#    Copyright (C) 2020-2021 by @InukaAsith
+#    Copyright (C) 2020-2021 by The-Quaint
 #    This programme is a part of Felicia TG bot project
 #
 #    This program is free software: you can redistribute it and/or modify
